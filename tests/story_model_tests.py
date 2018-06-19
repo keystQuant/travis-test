ddb8363d605f23b42d26ac6bcf439fbfd1d6b02e
1 <ppark9553@gmail.com>
2018-06-19 21:25:48 +0900

    adding one more test

--- tests/story_model_tests.py
+++ tests/story_model_tests.py
@@ -9,3 +9,11 @@ def test_app_can_create_story_data():
 
     num_of_creates = Story.objects.all().count()
     assert num_of_creates == 1
+
+@pytest.mark.django_db
+def test_app_again_can_create_story_data():
+    story_inst = Story(title='test title 2', story='test story 2')
+    story_inst.save()
+
+    num_of_creates = Story.objects.all().count()
+    assert num_of_creates == 1
